7a3cb94dc06434869e6d9c0a16f1a38b53dfc7fc
NgocDanh <duongngocdanh6@gmail.com>
2024-11-26 20:30:06 +0700

    feat: DONE index/ detail book page user

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,32 @@ var router = express.Router();
 const { Book, Author, Category } = require("../db")
 const { formatDateToDDMMYYYY } = require("../utils/Utils");
 
-router.get("/books/:id", async(req,res)=>{
+router.get("/books/:id", async (req, res) => {
   const bookId = req.params.id
-  const book = await Book.findOne({id: bookId});
+  const book = await Book.findOne({ id: bookId });
   const category = await Category.findOne({ id: book.id_category });
   const author = await Author.findOne({ id: book.id_author });
 
   const formattedBook = book.toObject();
   formattedBook.createdAt = formatDateToDDMMYYYY(book.createdAt);
 
+  const relatedBooks = await Book.find({ id_category: book.id_category, _id: { $ne: book._id }});
+
   const data = {
-      ...formattedBook,
-      category,
-      author,
+    ...formattedBook,
+    category,
+    author,
   }
   // res.json(data)
-  res.render("detail_book", { data: data})
+  res.render("detail_book", { data: data, relatedBooks: relatedBooks })
 })
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', async (req, res, next) => {
+  const books = await Book.find({isEnable: true})
+  res.render("index", {
+    books: books,
+  })
 });
-
-
 
 module.exports = router;
